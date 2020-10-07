f6c84ed27da88dd5a1307162fe441cbfe76e1d36
Inusah-Said <saidinusah29@gmail.com>
2020-10-07 04:01:35 +0000

    product service added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,10 +12,9 @@ import { ProductService } from "./product.service";
     AppComponent,
     ProductPageComponent,
     ProductDescriptionComponent,
-    ProductService,
   ],
   imports: [BrowserModule, FormsModule, HttpModule],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
